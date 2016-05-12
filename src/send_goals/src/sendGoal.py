3824fa1cc4cd1715ee7ce5009bee2ca8774c8fe6
Marko <markoprelevik@gmail.com>
2016-05-12 17:04:25 +0200

    mod sendGoal, added the map

--- src/send_goals/src/sendGoal.py
+++ src/send_goals/src/sendGoal.py
@@ -58,6 +58,7 @@ class SendGoal():
             sys.exit(0)
         if len(data.status_list) == 0:
             self.sendGoal()
+            return
         print len(data.status_list)
         stat = data.status_list[len(data.status_list) - 1].status
         # print self.nextID, stat
