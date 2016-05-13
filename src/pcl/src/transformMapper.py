4a18d891c5d2d3bb42bcd3812dabbe0f5b5ba0d4
Marko <markoprelevik@gmail.com>
2016-05-13 16:02:03 +0200

    indent problem.

--- src/pcl/src/transformMapper.py
+++ src/pcl/src/transformMapper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import roslib
+
 roslib.load_manifest('pcl')
 import rospy
 import sys, select, termios, tty, tf
@@ -17,33 +18,38 @@ import numpy
 
 # Node for face detection.
 class MarkerTransformer():
-
     def faces_callback(self, markers):
         n = len(markers)
         for i in xrange(0, n):
-        	tmp = markers[i]
-        	tmpPose = tmp.pose
-            try:
-                self.listener.waitForTransform(tmp.header.frame_id, '/map', tmp.header.stamp, rospy.Duration(4.5))
-                tmpPose = self.listener.transformPose('/map', tmpPose)
-            except tf.LookupException:
-                print "lookup"
-                continue
-            except tf.ConnectivityException:
-                print "conn"
-                continue
-            except tf.ExtrapolationException:
-                print "extrapolation"
-                continue
-            tmp.pose = tmpPose
-           	markers[i] = tmp
-        self.markers_pub.publish(markers)
+            tmp = markers[i]
+            tmpPose = tmp.pose
+        try:
+            self.listener.waitForTransform(tmp.header.frame_id, '/map', tmp.header.stamp, rospy.Duration(4.5))
+            tmpPose = self.listener.transformPose('/map', tmpPose)
+        except tf.LookupException:
+            print
+            "lookup"
+            continue
+        except tf.ConnectivityException:
+            print
+            "conn"
+            continue
+        except tf.ExtrapolationException:
+            print
+            "extrapolation"
+            continue
+        tmp.pose = tmpPose
+        markers[i] = tmp
 
-    def __init__(self):
-        self.joined_sub = message_filters.TimeSynchronizer([self.faces_sub, self.camera_sub], 30)
-        self.joined_sub.registerCallback(self.faces_callback)
-        self.markers_pub = rospy.Publisher('/pcl/transformedMarkers', MarkerArray)
-        self.listener = tf.TransformListener()
+    self.markers_pub.publish(markers)
+
+
+def __init__(self):
+    self.joined_sub = message_filters.TimeSynchronizer([self.faces_sub, self.camera_sub], 30)
+    self.joined_sub.registerCallback(self.faces_callback)
+    self.markers_pub = rospy.Publisher('/pcl/transformedMarkers', MarkerArray)
+    self.listener = tf.TransformListener()
+
 
 # Main function.
 if __name__ == '__main__':
