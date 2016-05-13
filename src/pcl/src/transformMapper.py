cfff4570ac92fbafe219d5e5490c6b05f9554815
Marko <markoprelevik@gmail.com>
2016-05-13 15:56:15 +0200

    transformer script, to test

---
+++ src/pcl/src/transformMapper.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import roslib
+roslib.load_manifest('pcl')
+import rospy
+import sys, select, termios, tty, tf
+from std_msgs.msg import String, Bool, ColorRGBA
+import sensor_msgs.msg
+import message_filters
+from facedetector.msg import Detection
+from localizer.srv import Localize
+from sensor_msgs.msg import CameraInfo
+from visualization_msgs.msg import Marker, MarkerArray
+from image_geometry import PinholeCameraModel
+from geometry_msgs.msg import Point, Vector3, PoseStamped
+import numpy
+
+
+# Node for face detection.
+class MarkerTransformer():
+
+    def faces_callback(self, markers):
+        n = len(markers)
+        for i in xrange(0, n):
+        	tmp = markers[i]
+        	tmpPose = tmp.pose
+            try:
+                self.listener.waitForTransform(tmp.header.frame_id, '/map', tmp.header.stamp, rospy.Duration(4.5))
+                tmpPose = self.listener.transformPose('/map', tmpPose)
+            except tf.LookupException:
+                print "lookup"
+                continue
+            except tf.ConnectivityException:
+                print "conn"
+                continue
+            except tf.ExtrapolationException:
+                print "extrapolation"
+                continue
+            tmp.pose = tmpPose
+           	markers[i] = tmp
+        self.markers_pub.publish(markers)
+
+    def __init__(self):
+        self.joined_sub = message_filters.TimeSynchronizer([self.faces_sub, self.camera_sub], 30)
+        self.joined_sub.registerCallback(self.faces_callback)
+        self.markers_pub = rospy.Publisher('/pcl/transformedMarkers', MarkerArray)
+        self.listener = tf.TransformListener()
+
+# Main function.
+if __name__ == '__main__':
+
+    rospy.init_node('facemapper')
+    try:
+        fd = FaceMapper()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
