c1d4308dcb589426ecdfa2ec2d2d3ca0fc52d78f
Jakob Salej <jakob.salej@gmail.com>
2016-06-05 16:01:22 +0200

    Testing sending goals.

--- src/send_goals/src/face_recognizer.py
+++ src/send_goals/src/face_recognizer.py
@@ -9,7 +9,7 @@ from visualization_msgs.msg import Marker, MarkerArray
 from sound_play.msg import SoundRequest
 import tf
 
-status = 0
+status = 0      # 0 = looking for the face, 1 = approaching the face, 2 = waiting for command
 
 voice_pub = None
 markers_pub = None
@@ -97,7 +97,7 @@ def speak_robot(str_speech):
 
 
 def recognized_face(data):
-    global markers_pub, detect_true, sound_sent, color, det_entry, faces_count, face_marker, search_name, status, cancel_pub
+    global markers_pub, detect_true, sound_sent, color, det_entry, faces_count, face_marker, search_name, status, cancel_pub, voice_pub
 
     name = data.data
     #print name
@@ -105,6 +105,7 @@ def recognized_face(data):
     #when we have the position, but looking for the rotation (TO-DO: wait for the detection first)
     if status == 1 and search_name == name:
         cancel_pub.publish("true")
+        voice_pub.publish(search_name + ", where would you like to go?")
         status = 2
     
     thresh = 15
@@ -227,6 +228,7 @@ def sign_detection(points):
         else:
             pass
 
+
 def recognized_sign(data):
     global signs_count, detect_sign_true, det_entry_sign, sign_detected_again, honk_pub, stop_pub, slow_pub, oneway_pub
 
@@ -297,7 +299,7 @@ def voice_action(data):
     #street_pub.publish(colour_street)
     
     #we reset everything because we received a new goal
-    cancel_pub.publish("true")
+    #cancel_pub.publish("true")
     sent_street = 0
     status = 0
 
@@ -334,8 +336,9 @@ def publish_faces(non):
         
     markers_pub.publish(markers)
     
-    if sent_street == 0 and colour_street != None:
+    if sent_street == 0 and colour_street != None and status == 0:
         print "sending goal!"
+        cancel_pub.publish("true")
         street_pub.publish(colour_street)
         sent_street = 1
 
